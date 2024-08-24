8b8f2c206fdfaf2c5b0a88b3e4c3362fbdb351a4
Konstantin <mkv2008@yandex.ru>
2024-08-24 11:56:24 +0300

    добавил ещё один logger1

--- bookServer/main.go
+++ bookServer/main.go
@@ -38,7 +38,11 @@ func main() {
 	handler := slog.NewTextHandler(os.Stdout, options)
 	logger := slog.New(handler)
 
+	handler1 := slog.NewTextHandler(file, options)
+	logger1 := slog.New(handler1)
+
 	r.Use(Logging(logger))
+	r.Use(Logging1(logger))
 
 	r.HandleFunc("/book", struct_p.GetBook).Methods(http.MethodGet)
 	r.HandleFunc("/book", struct_p.AddBook).Methods(http.MethodPost)
@@ -46,7 +50,7 @@ func main() {
 	r.HandleFunc("/book", struct_p.UpdateBook).Methods(http.MethodPut)
 	r.HandleFunc("/books", struct_p.AllBooks).Methods(http.MethodGet)
 
-	logger.Warn("сервер запущен")
+	logger1.Warn("сервер запущен")
 	fmt.Println("сервер запущен")
 	err = http.ListenAndServe("127.0.0.1:8080", r)
 	logger.Warn("сервер отключён")
@@ -58,6 +62,17 @@ func main() {
 }
 
 func Logging(logger *slog.Logger) mux.MiddlewareFunc {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			starter := time.Now()
+			logger.Info("Request", slog.String("uri", r.RequestURI), slog.String("remote_addr", r.RemoteAddr))
+			next.ServeHTTP(w, r)
+			logger.Info("Finished", slog.String("duration", time.Since(starter).String()))
+		})
+	}
+}
+
+func Logging1(logger *slog.Logger) mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			starter := time.Now()
