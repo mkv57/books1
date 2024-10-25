2d39525a17eba1d6a817b3ff5c66538c9c0a3178
Konstantin <mkv2008@yandex.ru>
2024-10-25 16:38:25 +0300

    c

--- migrate/000001_create.up.sql
+++ migrate/000001_create.up.sql
@@ -13,23 +13,6 @@ year INTEGER NOT NULL,
 created_at time,
 updated_at time
 );
-CREATE TABLE books1
-(
-id_id serial PRIMARY KEY,
-title_id VARCHAR (50) UNIQUE NOT NULL,
-year_id INTEGER NOT NULL
-);
-CREATE TABLE books4
-(
-id_id_4 serial PRIMARY KEY,
-title_id_4 VARCHAR (50) UNIQUE NOT NULL,
-year_id_4 INTEGER NOT NULL
-);
-CREATE TABLE books3
-(
-id_id_2 serial PRIMARY KEY,
-title_id_2 VARCHAR (50) UNIQUE NOT NULL,
-year_id_2 INTEGER NOT NULL
-);
+
 
 
